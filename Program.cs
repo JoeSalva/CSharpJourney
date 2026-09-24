233f5382c7d99d5f1ea9b38715e16b9aeef267bb
JoeSalva <salvagainchibu@gmail.com>
2026-09-24 21:30:54 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// This is my first program
+// Console.WriteLine("Hello, World!");
+
+// String myString;
+
+// myString = "Hello, World!";
+
+// String coolString = "GoodBye, World!";
+
+// String firstName = "John";
+// String lastName = "Doe";
+// String fullName = firstName + " " + lastName;
+
+// int my_int;
+
+// float myFloat;
+// double myDouble;
+
+// my_int = 5;
+// myFloat = 5.5f;
+// myDouble = 5.5d;
+
+// Console.WriteLine(my_int);
+// Console.WriteLine(myFloat);
+// Console.WriteLine(myDouble);
+
+
+// Console.WriteLine(fullName);
+
+// myString = Console.ReadLine();
+
+// Console.WriteLine(myString.Length);
+
+// int myInt = 5;
+// double myDouble = myInt;
+
+// Console.WriteLine($"Int = {myInt}");
+// Console.WriteLine($"Double = {myDouble}");
+
+// if (true)
+// {
+//     Console.WriteLine("This will always print");
+// }
+
+// if (!false)
+// {
+//     Console.WriteLine("This will never print");
+// }
+
+// int num = 1;
+
+// if (num < 5)
+// {
+//     Console.WriteLine("This number is bettween 1 and 5");
+// }
+// else if (num == 5)
+// {
+//     Console.WriteLine("This number is equal to 5");
+// }
+// else
+// {
+//     Console.WriteLine("The number is greater than 5");
+// }
+
+// To test operators in control flow
+
+// num = 3;
+
+// if (num >= 1 && num <= 5)
+// {
+//     Console.WriteLine("This number is less than 5");
+// }
+// else if (num == 5)
+// {
+//     Console.WriteLine("This number is equal to 5");
+// }
+// else
+// {
+//     Console.WriteLine("The number is greater than 5");
+// }
